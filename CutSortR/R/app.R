39c710ccc6b6935390631c1a81fd72af95081dda
tomoyukif <f.tomoyuki@okayama-u.ac.jp>
2022-09-05 19:16:39 +0900

    update 202200905

--- CutSortR/R/app.R
+++ CutSortR/R/app.R
@@ -1,40 +1,43 @@
 library(shiny)
+library(shinyFeedback)
 
-img_fmt <- c("jpg", "png", "tif")
+
 
 ui <- navbarPage("CutSortR",
-                 tabPanel("Image Slicer", 
+                 header = useShinyFeedback(),
+                 tabPanel("Image Slicer",
                           fluidPage(
                             titlePanel("Image Slicer"),
-                            
+
                             sidebarLayout(
                               sidebarPanel(
                                 fileInput("fn",
                                           label = "Input image file(s):",
                                           multiple = TRUE,
+                                          placeholder = "Select file(s)",
                                           accept = "image/*"),
-                                
+
                                 numericInput("width",
                                             label = "Sliced image width (px):",
                                             value = 100,
                                             min = 1),
-                                
+                                textOutput("invalid_width"),
+
                                 numericInput("height",
                                              label = "Sliced image height (px):",
                                              value = 100,
                                              min = 1),
-                                
+                                textOutput("invalid_height"),
+
                                 numericInput("step",
                                              label = "Sliding step size (px):",
                                              value = 100,
                                              min = 1),
-                                
-                                selectInput("fmt",
-                                            "Output image format:",
-                                            img_fmt),
+                                textOutput("invalid_step"),
+                                uiOutput("fmt"),
                                 ),
                               mainPanel(
-                                # textOutput("img_width")
+                                textOutput("img_width")
                               )
                             )
                           )),
@@ -42,9 +45,33 @@ ui <- navbarPage("CutSortR",
 )
 
 server <- function(input, output){
-  # if(input$fn == ""){
-  #   output$img_width <- 100
-  # }
-} 
+  img_width <- 100
+  img_height <- 100
+  img_minlen <- min(c(img_width, img_height))
+  img_fmt <- "png"
+
+  output$fmt <- renderUI({
+    selectInput("fmt",
+                "Output image format:",
+                c("jpg", "png", "tif"),
+                selected = img_fmt)
+  })
+
+  invalid_width <- reactive({
+    feedbackWarning("width", input$width > img_width, "Invalid width!")
+  })
+  output$invalid_width <- renderText(invalid_width())
+
+  invalid_height <- reactive({
+    feedbackWarning("height", input$height > img_height, "Invalid height!")
+  })
+  output$invalid_height <- renderText(invalid_height())
+
+  invalid_step <- reactive({
+    feedbackWarning("step", input$step > img_minlen, "Invalid step!")
+  })
+  output$invalid_step <- renderText(invalid_step())
+
+}
 
 shinyApp(ui = ui, server = server)
